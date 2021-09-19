d4c41043d14086194828a7e0c246f8c2759e6ac9
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-09-19 23:58:16 +0300

    added Readme file

---
+++ README.md
@@ -0,0 +1,28 @@
+## {Kipngeno Sigei}
+### Pezzeria
+## By Kipngeno Sigei 
+# Description
+This is my Pizzeria website that gives variaties of pizzas, shows their prices and sizes and how to place an order.
+## Setup/Installation Requirements
+To visit this site, You need to have the following;
+1. Internet bundles.
+2. Mobile/Computer browser (e.g chrome,mozila firefox etc.)
+## Known Bugs
+No known bugs so far.
+## Technologies Used
+# html
+# css
+# Bootstrap
+# JavaScript
+# Jquery
+## Support and contact details
+For surport and Questions, feel free to reach me on,
+E-mail:kipngenosigei04@gmail.com
+Phone No. +254725059416
+
+
+### License
+
+
+* [[License: MIT]](LICENSE.md)
+* Copyright (c) 2021 Kipngeno Sigei
--- js/index.js
+++ js/index.js
@@ -1,84 +1,77 @@
-const nav = document.querySelector('.nav')
-window.addEventListener('scroll', fixNav)
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
 
 function fixNav() {
-    if(window.scrollY > nav.offsetHeight + 150) {
-        nav.classList.add('active')
-    } else {
-        nav.classList.remove('active')
-    }
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
 }
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
 
-$(document).ready(function() {
-    $("#text-center").submit(function(event) {
-      function flavor() {
-        var pizzaFlavour = document.getElementById("flavor").value;
-        return parseInt(pizzaFlavour);
-      }
-      function size() {
-        var pizzaSize = document.getElementById("size").value;
-        return parseInt(pizzaSize);
-      }
-      function crust() {
-        var pizzaCrust = document.getElementById("crust").value;
-        return parseInt(pizzaCrust);
-      }
-      function topping() {
-        var pizzaTopping = document.getElementById("topping").value;
-        return parseInt(pizzaTopping);
-      }
-      function number() {
-        var pizzaNumber = document.getElementById("quantity").value;
-        return parseInt(pizzaNumber);
-      }
-  
-      //takes user's details
-      function Order(flavor, size, crust, topping, quantity) {
-        this.newFlavor = flavor;
-        this.newSize = size;
-        this.newCrust = crust;
-        this.newTopping = topping;
-        this.newQuantity = quantity;
-      }
-  
-      //saves the users order
-      var userInput = new Order(flavor(), size(), crust(), topping(), number());
-  
-      //calculates total expenditure of the user
-      var totalCost =
-        (userInput.newSize +
-          userInput.newCrust +
-          userInput.newTopping +
-          userInput.newFlavor) *
-        userInput.newQuantity;
-  
-      //prompts for the user
+$(document).ready(function () {
+  $("#text-center").submit(function (event) {
+    function flavor() {
+      var pizzaFlavour = document.getElementById("flavor").value;
+      return parseInt(pizzaFlavour);
+    }
+    function size() {
+      var pizzaSize = document.getElementById("size").value;
+      return parseInt(pizzaSize);
+    }
+    function crust() {
+      var pizzaCrust = document.getElementById("crust").value;
+      return parseInt(pizzaCrust);
+    }
+    function topping() {
+      var pizzaTopping = document.getElementById("topping").value;
+      return parseInt(pizzaTopping);
+    }
+    function number() {
+      var pizzaNumber = document.getElementById("quantity").value;
+      return parseInt(pizzaNumber);
+    }
 
-      alert("Your charges for Pizza is " + totalCost);
-      prompt("Please enter your email address");
-      prompt("Please enter your phone number");
-      prompt("Please enter your location");
-      alert("Your pizza will be delivered.Delivery fee is Ksh. 150");
-      alert("Thank you for believing in us.")
+    function Order(flavor, size, crust, topping, quantity) {
+      this.newFlavor = flavor;
+      this.newSize = size;
+      this.newCrust = crust;
+      this.newTopping = topping;
+      this.newQuantity = quantity;
+    }
 
-  
-      //a method to reset the form after all operations have been completed
-      $("#text-center").reset();
-  
-      event.preventDefault();
-    });
+    var userInput = new Order(flavor(), size(), crust(), topping(), number());
+
+    var totalCost =
+      (userInput.newSize +
+        userInput.newCrust +
+        userInput.newTopping +
+        userInput.newFlavor) *
+      userInput.newQuantity;
+
+    alert("Your charges for Pizza is " + totalCost);
+    prompt("Please enter your email address");
+    prompt("Please enter your phone number");
+    prompt("Please enter your location");
+    alert("Your pizza will be delivered.Delivery fee is Ksh. 150");
+    alert("Thank you for believing in us.");
+
+    $("#text-center").reset();
+
+    event.preventDefault();
   });
+});
